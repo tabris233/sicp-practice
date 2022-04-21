b7a4b13a50b740b76c898dd48f5dfb08b159f23e
tabris <tabris.dq@qq.com>
2022-04-22 01:35:36 +0800

    upd: 1.43

--- sicp/1.1/practice1.43.rkt
+++ sicp/1.1/practice1.43.rkt
@@ -13,8 +13,34 @@
     (try 1))
 
 ; TODO 这里可以用倍增的方法做，更快一点。
-; (define )
+(define (even? n)
+  (= (remainder n 2) 0))
 
+(define (compose f g)
+    (lambda (x) 
+        (f (g x))))
+
+(define (fast-repeated f k)
+    (define (try g h n)
+        (if (= n 0)
+            h
+            (if (even? n)
+                (try g (compose h g) (/ n 2))
+                (try (compose g g) h (- n 1)))))  ; n is odd here so we need to subtract 1
+    
+    (try f f k))
+
+(display ((repeated square 1) 5))
+(newline)
+(display ((repeated square 2) 5))
+(newline)
+(display ((repeated square 3) 5))
+(newline)
+(display ((repeated square 4) 5))
+(newline)
+
+(display "---------------------")
+(newline)
 (display ((repeated square 1) 5))
 (newline)
 (display ((repeated square 2) 5))
